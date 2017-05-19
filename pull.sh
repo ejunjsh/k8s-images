676d25577d031c067354ef2d511714414ab9b7d2
zhouff <sjj050121014@163.com>
2017-05-19 23:26:52 +0800

    ok

--- pull.sh
+++ pull.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 images=(etcd-amd64:3.0.17 k8s-dns-dnsmasq-nanny-amd64:1.14.1 k8s-dns-kube-dns-amd64:1.14.1 k8s-dns-sidecar-amd64:1.14.1 kube-apiserver-amd64:v1.6.0 kube-controller-manager-amd64:v1.6.0 kube-proxy-amd64:v1.6.0 kube-scheduler-amd64:v1.6.0 pause-amd64:3.0)
 for imageName in ${images[@]} ; do
   docker pull sjj050121014/$imageName
